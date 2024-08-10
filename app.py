c66d64adea5c52737ab75a7c6a0024e7765b7a42
Shorya Sharma <shoryamathura22@gmail.com>
2024-08-10 21:15:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,17 +15,8 @@ hf_token = "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"
 import huggingface_hub
 huggingface_hub.login(token=hf_token)
 
-
-# model= AutoModelForSeq2SeqLM.from_pretrained("Shorya22/BART-Large-Fine_Tunned")
-# tokenizer= AutoTokenizer.from_pretrained("Shorya22/BART-Large-Fine_Tunned")
-
-@st.cache(max_entries=100, ttl=3600, persist=True, max_size=1e9)
-def load_model_and_tokenizer():
-    model = AutoModelForSeq2SeqLM.from_pretrained("Shorya22/BART-Large-Fine_Tunned")
-    tokenizer = AutoTokenizer.from_pretrained("Shorya22/BART-Large-Fine_Tunned", use_fast=True)
-    return model, tokenizer
-
-model, tokenizer = load_model_and_tokenizer()
+model = AutoModelForSeq2SeqLM.from_pretrained("Shorya22/BART-Large-Fine_Tunned")
+tokenizer = AutoTokenizer.from_pretrained("Shorya22/BART-Large-Fine_Tunned", use_fast=True)
 
 # Define the prompt template
 prompt = PromptTemplate(template="Summarize the following text\n\n{input_text}\n\nSummary:\n\n", input_variables=['input_text'])
@@ -34,7 +25,6 @@ pipe= pipeline(task='text2text-generation',model=model,tokenizer=tokenizer,max_n
 llm= HuggingFacePipeline(pipeline=pipe)
 
 chain= prompt | llm
-
 
 st.header("Hello!")
 st.title("Text Summarization Application",)
